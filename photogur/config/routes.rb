07d6e1445b18442e1a56793f5c680324c16ffb92
amber-h <amber.houle3@gmail.com>
2013-09-03 21:03:54 -0400

    Making each picture show at it's own URL using static routes

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -2,6 +2,10 @@ Photogur::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
     get 'pictures' => 'pictures#index'
+
+    get 'pictures/0' => 'pictures#picture0'
+    get 'pictures/1' => 'pictures#picture1'
+    get 'pictures/2' => 'pictures#picture2'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
